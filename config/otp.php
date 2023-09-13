2a5800b30748d84c5c8059c96c55bb6962982d51
Sadiq Salau <sadiqsalau888@gmail.com>
2023-09-13 15:01:02 +0100

    Removed unused config key Removed external package link

--- config/otp.php
+++ config/otp.php
@@ -1,36 +1,12 @@
 <?php
 
-use SadiqSalau\LaravelOtp\Stores\CacheStore;
-use SadiqSalau\LaravelOtp\Stores\SessionStore;
-
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | OTP store
-    |--------------------------------------------------------------------------
-    |
-    | Store for OTP
-    |
-    */
-    'store' => CacheStore::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | OTP key
-    |--------------------------------------------------------------------------
-    |
-    | Key used to store and retrieve OTP
-    |
-    */
-    'store_key' => env('OTP_STORE_KEY', 'otp'),
-
     /*
     |--------------------------------------------------------------------------
     | OTP format
     |--------------------------------------------------------------------------
     |
     | Can be one of alpha, alphanumeric, numeric
-    | See: https://github.com/Hi-Folks/rando-php#random-string
     |
     */
     'format' => env('OTP_FORMAT', 'numeric'),
